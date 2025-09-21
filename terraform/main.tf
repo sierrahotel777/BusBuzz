730558acff89d3b6d32d0cbfc827b04bf5a478ed
sierrahotel777 <sierrahotel777@gmail.com>
2025-09-22 00:43:09 +0530

    Removed duplicate outputs teraaform

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,12 +77,3 @@ resource "azurerm_app_service_source_control" "backend_source" {
   branch     = "main"
   use_manual_integration = true
 }
-
-# Outputs
-output "frontend_url" {
-  value = azurerm_linux_web_app.frontend_app.default_hostname
-}
-
-output "backend_url" {
-  value = azurerm_linux_web_app.backend_app.default_hostname
-}
